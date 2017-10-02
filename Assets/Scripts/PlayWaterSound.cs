f1ea460e6e27d78e82cb44654314ee25fe731c65
SLCTECH\dalbuquerquedepaul <demetrio.ap@gmail.com>
2017-10-02 10:38:02 -0400

    First version of Amazing Racer

---
+++ Assets/Scripts/PlayWaterSound.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayWaterSound : MonoBehaviour
+{
+	void OnTriggerEnter(Collider other)
+	{
+		AudioSource audio = GetComponent<AudioSource>();
+		audio.Play();
+		audio.Play(44100);
+
+	}
+}
